9adb263d670fd0b1be9e853321694b620b880e77
louiscklaw <louiscklaw@users.noreply.github.com>
2023-01-09 18:22:37 +0800

    update,

---
+++ rakuten-travel-tryout/pages/hkg/zh-hk/CompanyLogo.jsx
@@ -0,0 +1,11 @@
+import Box from "@mui/material/Box";
+
+export default (props) => {
+  return (
+    <>
+      <Box {...props}>
+        <img src="/img/Rakuten.webp" width="auto" height="100px" {...props} />
+      </Box>
+    </>
+  );
+};
--- rakuten-travel-tryout/styles/theme/lightTheme.js
+++ rakuten-travel-tryout/styles/theme/lightTheme.js
@@ -3,6 +3,7 @@ import { createTheme } from '@mui/material/styles';
 const lightTheme = createTheme({
   palette: {
     mode: 'light',
+    
   },
 });
 
