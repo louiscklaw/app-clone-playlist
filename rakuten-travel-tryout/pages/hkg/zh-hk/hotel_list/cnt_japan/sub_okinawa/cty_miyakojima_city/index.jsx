5eb9c914802af925d9860babe93802b9a0e1fc95
louiscklaw <louiscklaw@users.noreply.github.com>
2023-01-11 00:00:50 +0800

    update,

--- rakuten-travel-tryout/pages/hkg/zh-hk/hotel_list/cnt_japan/sub_okinawa/cty_miyakojima_city/index.jsx
+++ rakuten-travel-tryout/pages/hkg/zh-hk/hotel_list/cnt_japan/sub_okinawa/cty_miyakojima_city/index.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import Image from "next/image";
+import { styled, alpha } from "@mui/material/styles";
 
 import AppBar from "@mui/material/AppBar";
 import Stack from "@mui/material/Stack";
@@ -7,6 +8,7 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
+import Rating from "@mui/material/Rating";
 
 import MenuIcon from "@mui/icons-material/Menu";
 import PublicIcon from "@mui/icons-material/Public";
@@ -24,9 +26,9 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import Link from "next/link";
 import SearchIcon from "@mui/icons-material/Search";
-
+import DoneIcon from "@mui/icons-material/Done";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
-
+import FmdGoodIcon from "@mui/icons-material/FmdGood";
 import {
   Grid,
   Slider,
@@ -37,6 +39,7 @@ import {
   TextField,
   Alert,
   Button,
+  Switch,
   ButtonGroup,
   Modal,
   Popover,
@@ -47,7 +50,13 @@ import {
   ListItemIcon,
   ListItemText,
   Divider,
+  MenuItem,
+  CircularProgress,
+  Chip,
+  Card,
 } from "@mui/material";
+import Sun from "@mui/icons-material/LightMode";
+
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import MapIcon from "@mui/icons-material/Map";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
@@ -55,6 +64,9 @@ import MailIcon from "@mui/icons-material/Mail";
 import CloseIcon from "@mui/icons-material/Close";
 import AliceCarousel from "react-alice-carousel";
 import ConfirmationNumberIcon from "@mui/icons-material/ConfirmationNumber";
+import EditIcon from "@mui/icons-material/Edit";
+import Menu from "@mui/material/Menu";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 // import RakutenWebp from '/assets/Rakuten.webp'
 import promotionsAndDiscounts from "pages/api/promotionsAndDiscounts";
@@ -74,6 +86,7 @@ import color from "pages/hkg/zh-hk/color";
 import Checkbox from "@mui/material/Checkbox";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
+import CircularProgressWithLabel from "./CircularProgressWithLabel";
 
 const AskLink = React.forwardRef(({ onClick, href }, ref) => {
   return (
@@ -85,6 +98,168 @@ const AskLink = React.forwardRef(({ onClick, href }, ref) => {
 
 function valuetext(value) {
   return `${value}°C`;
+}
+
+const StyledMenu = styled((props) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: "bottom",
+      horizontal: "left",
+    }}
+    transformOrigin={{
+      vertical: "top",
+      horizontal: "left",
+    }}
+    {...props}
+  />
+))(({ theme }) => ({
+  "& .MuiPaper-root": {
+    borderRadius: 6,
+    marginTop: theme.spacing(1),
+    minWidth: 180,
+    color:
+      theme.palette.mode === "light"
+        ? "rgb(55, 65, 81)"
+        : theme.palette.grey[300],
+    boxShadow:
+      "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
+    "& .MuiMenu-list": {
+      padding: "4px 0",
+    },
+    "& .MuiMenuItem-root": {
+      "& .MuiSvgIcon-root": {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      "&:active": {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity
+        ),
+      },
+    },
+  },
+}));
+
+const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+  width: 62,
+  height: 34,
+  padding: 7,
+  "& .MuiSwitch-switchBase": {
+    margin: 1,
+    padding: 0,
+    transform: "translateX(6px)",
+    "&.Mui-checked": {
+      color: "#fff",
+      transform: "translateX(22px)",
+      "& .MuiSwitch-thumb:before": {
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+          "#fff"
+        )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+      },
+      "& + .MuiSwitch-track": {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
+      },
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    backgroundColor: theme.palette.mode === "dark" ? "#003892" : "#001e3c",
+    width: 32,
+    height: 32,
+    "&:before": {
+      content: "''",
+      position: "absolute",
+      width: "100%",
+      height: "100%",
+      left: 0,
+      top: 0,
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center",
+      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+        "#fff"
+      )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+    },
+  },
+  "& .MuiSwitch-track": {
+    opacity: 1,
+    backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
+    borderRadius: 20 / 2,
+  },
+}));
+
+function HotelPost1() {
+  return <Box></Box>;
+}
+
+function HotelPost() {
+  return (
+    <>
+      <Card sx={{ padding: "1rem" }}>
+        <Grid container>
+          <Grid item xs={5}>
+            <img src="https://trvis.r10s.com/d/strg/ctrl/26/a1ee75551cc8f56e1489078d18f56e44a4d73679.47.1.26.2.jpg?resize=300:*" />
+          </Grid>
+          <Grid item xs={4}>
+            <Rating name="read-only" value={4} readOnly />
+            <Typography variant="body1" fontWeight="bold">
+              宮古島東急酒店與度假村
+            </Typography>
+            <Chip
+              label="新冠肺炎相關措施"
+              variant="outlined"
+              color="success"
+              size="small"
+            />
+
+            <Stack sx={{ color: color.textBlack }} spacing={1}>
+              <Stack direction="row" spacing={0.5} alignItems="center">
+                <DoneIcon sx={{ fontSize: "1rem" }} />
+                <Typography variant="body2">一人旅行 </Typography>
+              </Stack>
+              <Stack direction="row" spacing={0.5} alignItems="center">
+                <DoneIcon sx={{ fontSize: "1rem" }} />
+                <Typography variant="body2">免費停車場 </Typography>
+              </Stack>
+              <Stack direction="row" spacing={0.5} alignItems="center">
+                <DoneIcon sx={{ fontSize: "1rem" }} />
+                <Typography variant="body2">適合家族 </Typography>
+              </Stack>
+              <Stack direction="row" spacing={0.5} alignItems="center">
+                <FmdGoodIcon sx={{ fontSize: "1rem" }} />
+                <Typography variant="body2">
+                  由宮古機場駕車約 16 分鐘可達
+                </Typography>
+              </Stack>
+            </Stack>
+          </Grid>
+          <Grid item xs={3}>
+            4.6
+            <CircularProgressWithLabel value={4.6} />
+            <Typography variant="h6">非常好</Typography>
+            <Typography variant="body2">45 則評價</Typography>
+            <Typography variant="body1" fontWeight="bold">
+              $1,369.77~
+            </Typography>
+            <Typography variant="body2" sx={{ color: color.textDeepGrey }}>
+              每間房
+            </Typography>
+            <Typography variant="body2" sx={{ color: color.textDeepGrey }}>
+              2 位住客 1 晚
+            </Typography>
+            <Button
+              variant="contained"
+              sx={{ backgroundColor: color.accentGreen }}
+            >
+              查看客房和住宿計劃
+            </Button>
+          </Grid>
+        </Grid>
+      </Card>
+    </>
+  );
 }
 
 export default () => {
@@ -259,6 +434,17 @@ export default () => {
   // lang-select-popover
   let [app_bar_height, setAppBarHeight] = React.useState(0);
   let [scrolled, setScrolled] = React.useState(false);
+
+  // suggested menu
+  const [suggested_anchor_el, setSuggestedAnchorEl] = React.useState(null);
+  const suggested_open = Boolean(suggested_anchor_el);
+  const handleSuggestedClick = (event) => {
+    setSuggestedAnchorEl(event.currentTarget);
+  };
+  const handleSuggestedClose = () => {
+    setSuggestedAnchorEl(null);
+  };
+  // suggested menu
 
   React.useEffect(() => {
     // document.querySelector("#right_menu")?.click();
@@ -529,8 +715,8 @@ export default () => {
           paddingRight: { xs: "3vw", xl: "10vw" },
         }}
       >
-        <Box sx={{ width: "80%", marginTop: "1rem", marginBottom: "3rem" }}>
-          <Grid container>
+        <Box sx={{ marginTop: "1rem", marginBottom: "3rem" }}>
+          <Grid container sx={{}}>
             <Grid item xs={4} sx={{}}>
               <Typography
                 variant="subtitle1"
@@ -723,22 +909,67 @@ export default () => {
                 </Typography>
               </Box>
             </Grid>
-            <Grid item xs={8} sx={{ backgroundColor: "tomato" }}>
-              <Box>
-                推薦 地圖 宮古島東急酒店與度假村 新冠肺炎相關措施 一人旅行
-                免費停車場 適合家族 由宮古機場駕車約 16 分鐘可達 4.6 非常好 45
-                則評價 $1,369.77~ 每間房 2 位住客 1 晚 查看客房和住宿計劃
-              </Box>
-              <Box>
-                推薦 地圖 宮古島東急酒店與度假村 新冠肺炎相關措施 一人旅行
-                免費停車場 適合家族 由宮古機場駕車約 16 分鐘可達 4.6 非常好 45
-                則評價 $1,369.77~ 每間房 2 位住客 1 晚 查看客房和住宿計劃
-              </Box>
-              <Box>
-                推薦 地圖 宮古島東急酒店與度假村 新冠肺炎相關措施 一人旅行
-                免費停車場 適合家族 由宮古機場駕車約 16 分鐘可達 4.6 非常好 45
-                則評價 $1,369.77~ 每間房 2 位住客 1 晚 查看客房和住宿計劃
-              </Box>
+            <Grid item xs={8} sx={{}}>
+              <StyledMenu
+                id="demo-customized-menu"
+                MenuListProps={{ "aria-labelledby": "demo-customized-button" }}
+                anchorEl={suggested_anchor_el}
+                open={suggested_open}
+                onClose={handleSuggestedClose}
+              >
+                <MenuItem onClick={handleSuggestedClose} disableRipple>
+                  <EditIcon /> 推薦價格（低至高）
+                </MenuItem>
+                <MenuItem onClick={handleSuggestedClose} disableRipple>
+                  <EditIcon /> 價格（高至低）
+                </MenuItem>
+                <MenuItem onClick={handleSuggestedClose} disableRipple>
+                  <EditIcon /> 與景點距離（近至遠）
+                </MenuItem>
+                <MenuItem onClick={handleSuggestedClose} disableRipple>
+                  <EditIcon /> 星級評分（低至高）
+                </MenuItem>
+                <MenuItem onClick={handleSuggestedClose} disableRipple>
+                  <EditIcon /> 星級評分（高至低）
+                </MenuItem>
+                <MenuItem onClick={handleSuggestedClose} disableRipple>
+                  <EditIcon /> 住客評價分數（高至低）
+                </MenuItem>
+              </StyledMenu>
+
+              <Stack direction="row">
+                <Button
+                  id="demo-customized-button"
+                  aria-controls={
+                    suggested_open ? "demo-customized-menu" : undefined
+                  }
+                  aria-haspopup="true"
+                  aria-expanded={suggested_open ? "true" : undefined}
+                  variant="contained"
+                  disableElevation
+                  onClick={handleSuggestedClick}
+                  endIcon={<KeyboardArrowDownIcon />}
+                >
+                  推薦
+                </Button>
+
+                <FormControlLabel
+                  control={<MaterialUISwitch sx={{ m: 1 }} defaultChecked />}
+                  label="地圖"
+                />
+              </Stack>
+
+              <Stack
+                direction="column"
+                spacing={3}
+                sx={{ paddingTop: "1rem", paddingBottom: "1rem" }}
+              >
+                <HotelPost />
+                <HotelPost />
+                <HotelPost />
+                <HotelPost />
+                <HotelPost />
+              </Stack>
             </Grid>
           </Grid>
         </Box>
