823823dddd5bc97c8a92c4f209cc296c2ebbe63e
louiscklaw <louiscklaw@users.noreply.github.com>
2023-01-15 19:06:56 +0800

    update,

--- rakuten-travel-tryout/frontend/next.config.js
+++ rakuten-travel-tryout/frontend/next.config.js
@@ -5,15 +5,13 @@ module.exports = {
       {
         protocol: 'https',
         hostname: 'example.com',
-        port: '',
         pathname: '/account123/**',
-      }, 
+      },
       {
         protocol: 'https',
         hostname: 'upload.wikimedia.org',
-        port: '',
         pathname: '/**',
-      },     
+      },
     ],
   },
-}
+};
