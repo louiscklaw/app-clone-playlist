e66da1ccde9745a3f5e0f16d1e0b0d2d5d64c86a
louiscklaw <louiscklaw@users.noreply.github.com>
2023-01-15 19:07:37 +0800

    update,

--- rakuten-travel-tryout/frontend/pages/hkg/zh-hk/hotel_info_item/cnt_japan/sub_okinawa/cty_miyakojima_city/10123456795958/index.jsx
+++ rakuten-travel-tryout/frontend/pages/hkg/zh-hk/hotel_info_item/cnt_japan/sub_okinawa/cty_miyakojima_city/10123456795958/index.jsx
@@ -1,47 +1,61 @@
-import * as React from "react";
+import * as React from 'react';
 
-import { Box } from "@mui/material";
+import { Box } from '@mui/material';
 // import RakutenWebp from '/assets/Rakuten.webp'
 // import AliceCarouselWrap from "./AliceCarouselWrap";
 
-import AppBar from "components/AppBar";
-import BottomDownloadApp from "components/BottomDownloadApp";
-import BottomLink from "components/BottomLink";
-import BottomBreadcrumbs from "components/BottomBreadcrumbs";
-import AwardPlate from "components/HotelInfoItem/AwardPlate";
-import CovinPlate from "components/HotelInfoItem/CovinPlate";
-import FacilitiesPlate from "components/HotelInfoItem/FacilitiesPlate";
-import ResidentInfoPlate from "components/HotelInfoItem/ResidentInfoPlate";
-import SearchPlan from "components/HotelInfoItem/SearchPlan";
-import SpecialsPlate from "components/HotelInfoItem/SpecialsPlate";
-import TopPlate from "components/HotelInfoItem/TopPlate";
-import MarksAndComments from "components/MarksAndComments";
+import AppBar from 'components/AppBar';
+import BottomDownloadApp from 'components/BottomDownloadApp';
+import BottomLink from 'components/BottomLink';
+import BottomBreadcrumbs from 'components/BottomBreadcrumbs';
+import AwardPlate from 'components/HotelInfoItem/AwardPlate';
+import CovinPlate from 'components/HotelInfoItem/CovinPlate';
+import FacilitiesPlate from 'components/HotelInfoItem/FacilitiesPlate';
+import ResidentInfoPlate from 'components/HotelInfoItem/ResidentInfoPlate';
+import SearchPlan from 'components/HotelInfoItem/SearchPlan';
+import SpecialsPlate from 'components/HotelInfoItem/SpecialsPlate';
+import TopPlate from 'components/HotelInfoItem/TopPlate';
+import MarksAndComments from 'components/MarksAndComments';
+import { useEffect } from 'react';
+import { getHotelInfo } from 'pages/api/hotel_info';
 
 export default () => {
   const [value, setValue] = React.useState(0);
+  const [hotel_info, setHotelInfo] = React.useState({});
+
+  useEffect(() => {
+    getHotelInfo({ hotel_id: 1 })
+      .then(res => res.json())
+      .then(res_json => {
+        console.log('helloworld');
+        setHotelInfo(res_json[0]);
+      });
+  }, []);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
+  if (hotel_info === {}) return <>loading</>;
 
   return (
     <>
       <AppBar />
       <Box
         sx={{
-          paddingLeft: { xs: "3vw", xl: "10vw" },
-          paddingRight: { xs: "3vw", xl: "10vw" },
+          paddingLeft: { xs: '3vw', xl: '10vw' },
+          paddingRight: { xs: '3vw', xl: '10vw' },
         }}
       >
         <div class="column">
-          <TopPlate sx={{ display: "none" }} />
-          <SearchPlan style={{ display: "none" }} />
-          <MarksAndComments />
-          <SpecialsPlate style={{ display: "none" }} />
-          <ResidentInfoPlate style={{ display: "none" }} />
-          <CovinPlate style={{ display: "none" }} />
-          <AwardPlate style={{ display: "none" }} />
-          <FacilitiesPlate style={{ display: "none" }} />
+          <TopPlate sx={{}} hotel_info={hotel_info} />
+          <SearchPlan style={{ display: 'none' }} />
+          <MarksAndComments style={{ display: 'none' }} />
+          <SpecialsPlate style={{ display: 'none' }} />
+          <ResidentInfoPlate style={{ display: 'none' }} />
+          <CovinPlate style={{ display: 'none' }} />
+          <AwardPlate style={{ display: 'none' }} />
+          <FacilitiesPlate style={{ display: 'none' }} />
         </div>
       </Box>
       <BottomBreadcrumbs />
