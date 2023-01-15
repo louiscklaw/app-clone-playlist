7d8b7314ea29412307592c44325e592e836783c4
louiscklaw <louiscklaw@users.noreply.github.com>
2023-01-15 19:07:46 +0800

    update,

--- rakuten-travel-tryout/frontend/components/HotelInfoItem/ResidentInfoPlate.jsx
+++ rakuten-travel-tryout/frontend/components/HotelInfoItem/ResidentInfoPlate.jsx
@@ -1,50 +1,49 @@
-import {
-  Box,
-  Button,
-  Chip,
-  Divider,
-  Grid,
-  IconButton,
-  Stack,
-  Typography,
-} from "@mui/material";
-import CircularProgressWithLabel from "components/CircularProgressWithLabel";
-import color from "pages/hkg/zh-hk/color";
-import DoneIcon from "@mui/icons-material/Done";
-import InsertPhotoIcon from "@mui/icons-material/InsertPhoto";
-import ShareIcon from "@mui/icons-material/Share";
-import CreditCardIcon from "@mui/icons-material/CreditCard";
-import covin_action from "pages/api/covin_action";
-import hotel_info from "pages/api/hotel_info";
+import { Box, Button, Chip, Divider, Grid, IconButton, Stack, Typography } from '@mui/material';
+import CircularProgressWithLabel from 'components/CircularProgressWithLabel';
+import color from 'pages/hkg/zh-hk/color';
+import DoneIcon from '@mui/icons-material/Done';
+import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
+import ShareIcon from '@mui/icons-material/Share';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import covin_action from 'pages/api/covin_action';
+import hotel_info, { getHotelInfo } from 'pages/api/hotel_info';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-export default (props) => {
+export default props => {
+  let [hotel_info, setHotelInfo] = useState({});
+
+  useEffect(() => {
+    getHotelInfo({ hotel_id: 1 })
+      .then(res => res.json())
+      .then(res_json => setHotelInfo(res_json));
+  }, []);
+
+  if (hotel_info == {}) return <>loading</>;
+
+  return <></>;
+
   return (
     <>
       <div {...props}>
-        <Stack sx={{ width: "100%" }} spacing={3}>
-          <Typography variant="h5" fontWeight={"bold"} sx={{}}>
+        <Stack sx={{ width: '100%' }} spacing={3}>
+          <Typography variant="h5" fontWeight={'bold'} sx={{}}>
             住宿設施資訊
           </Typography>
           <Divider />
           <Stack direction="row">
-            <Box sx={{ width: "50%" }}>
+            <Box sx={{ width: '50%' }}>
               <Stack direction="column" spacing={1}>
                 <Typography variant="body2">度假村酒店</Typography>
                 <Typography variant="body2">
-                  這棟別墅是別墅風格的夏威夷公寓出租。 所有 12
-                  棟別墅均設有溫水游泳池，私密性強。
-                  廚房、家具、家電一應俱全，可長期入住。
-                  在您逗留期間，這將是一個只屬於您的私人空間，所以請隨意使用。
+                  這棟別墅是別墅風格的夏威夷公寓出租。 所有 12 棟別墅均設有溫水游泳池，私密性強。
+                  廚房、家具、家電一應俱全，可長期入住。 在您逗留期間，這將是一個只屬於您的私人空間，所以請隨意使用。
                 </Typography>
                 <div>
                   {hotel_info.map(({ name, value }) => (
-                    <Stack
-                      direction="row"
-                      sx={{ paddingBottom: "0.5rem" }}
-                      spacing={1}
-                    >
+                    <Stack direction="row" sx={{ paddingBottom: '0.5rem' }} spacing={1}>
                       <DoneIcon />
-                      <Typography variant="body1" sx={{ width: "150px" }}>
+                      <Typography variant="body1" sx={{ width: '150px' }}>
                         {name}
                       </Typography>
                       <Typography variant="body1" sx={{ flexGrow: 6 }}>
@@ -61,12 +60,12 @@ export default (props) => {
                 style={{
                   backgroundImage:
                     'url("https://media.wired.com/photos/59269cd37034dc5f91bec0f1/191:100/w_1280,c_limit/GoogleMapTA.jpg")',
-                  backgroundSize: "cover",
-                  backgroundPosition: "center",
-                  backgroundRepeat: "no-repeat",
-                  width: "100%",
-                  height: "100%",
-                  borderRadius: "1rem",
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                  backgroundRepeat: 'no-repeat',
+                  width: '100%',
+                  height: '100%',
+                  borderRadius: '1rem',
                 }}
               ></div>
             </Box>
--- rakuten-travel-tryout/frontend/components/HotelInfoItem/TopPlate.jsx
+++ rakuten-travel-tryout/frontend/components/HotelInfoItem/TopPlate.jsx
@@ -1,38 +1,36 @@
-import {
-  Box,
-  Button,
-  Chip,
-  Divider,
-  IconButton,
-  Stack,
-  Typography,
-} from "@mui/material";
-import CircularProgressWithLabel from "components/CircularProgressWithLabel";
-import color from "pages/hkg/zh-hk/color";
-import DoneIcon from "@mui/icons-material/Done";
-import InsertPhotoIcon from "@mui/icons-material/InsertPhoto";
-import ShareIcon from "@mui/icons-material/Share";
+import { Box, Button, Chip, Divider, IconButton, Stack, Typography } from '@mui/material';
+import CircularProgressWithLabel from 'components/CircularProgressWithLabel';
+import color from 'pages/hkg/zh-hk/color';
+import DoneIcon from '@mui/icons-material/Done';
+import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
+import ShareIcon from '@mui/icons-material/Share';
 
-export default ({ sx }) => {
+export default ({ sx, hotel_info }) => {
+  console.group();
+  console.dir(hotel_info.awards);
+  console.groupEnd();
+
+  if (!hotel_info) return <></>;
   return (
     <>
+      {JSON.stringify(hotel_info, null, 2)}
       <div class="row">
         <Box sx={{ flexGrow: 1 }}>
           <Stack direction="column" spacing={2}>
             <Typography variant="h6" fontWeight="bold">
-              宮古島上野費利斯別墅套房酒店
+              {hotel_info?.name}
             </Typography>
             <Stack direction="row" alignItems="center">
               <Chip label="度假村酒店" />
-              <Typography variant="subtitle2" fontSize="bold">
-                Rakuten Travel Gold Award 2021
-              </Typography>
+              <pre>{JSON.stringify(hotel_info.awards, null, 2)}</pre>
+              {JSON.stringify(hotel_info.awards, null, 2)}
+              {hotel_info.awards?.map(a => {
+                <Typography variant="subtitle2" fontSize="bold">
+                  {a.name}
+                </Typography>;
+              })}
             </Stack>
-            <Typography
-              variant="subtitle2"
-              fontWeight="bold"
-              sx={{ color: color.textColor6 }}
-            >
+            <Typography variant="subtitle2" fontWeight="bold" sx={{ color: color.textColor6 }}>
               設有私人溫水游泳池和廚房的別墅度假村
             </Typography>
           </Stack>
@@ -42,54 +40,54 @@ export default ({ sx }) => {
         </IconButton>
       </div>
 
-      <div class="row" style={{ gap: "1rem", ...sx }}>
-        <Box sx={{ width: "55%", borderRadius: "1rem" }}>
-          <Stack direction="column" spacing={0.5} sx={{ height: "100%" }}>
+      <div class="row" style={{ gap: '1rem', ...sx }}>
+        <Box sx={{ width: '55%', borderRadius: '1rem' }}>
+          <Stack direction="column" spacing={0.5} sx={{ height: '100%' }}>
             <Box
               sx={{
                 // backgroundColor: "gold",
                 backgroundImage: `url('https://trvis.r10s.com/d/strg/ctrl/26/437b86622a9af43c03f2628882661486821b7879.47.9.26.3.jpg')`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                borderRadius: "1rem 1rem 0 0 ",
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                borderRadius: '1rem 1rem 0 0 ',
                 flexGrow: 1,
               }}
             ></Box>
-            <Stack direction="row" spacing={0.5} sx={{ height: "100px" }}>
+            <Stack direction="row" spacing={0.5} sx={{ height: '100px' }}>
               <Box
                 sx={{
                   // backgroundColor: "green",
                   backgroundImage: `url('https://trvis.r10s.com/d/strg/ctrl/26/91d046af12015df16c75e62ab5eebe05154f4a0b.47.1.26.2.jpg')`,
-                  backgroundSize: "cover",
-                  backgroundPosition: "center",
-                  height: "100px",
-                  width: "20%",
-                  borderRadius: "0 0 0 1rem",
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                  height: '100px',
+                  width: '20%',
+                  borderRadius: '0 0 0 1rem',
                 }}
               ></Box>
               <Box
                 sx={{
-                  backgroundColor: "green",
-                  height: "100px",
-                  width: "20%",
+                  backgroundColor: 'green',
+                  height: '100px',
+                  width: '20%',
                 }}
               >
                 2
               </Box>
               <Box
                 sx={{
-                  backgroundColor: "green",
-                  height: "100px",
-                  width: "20%",
+                  backgroundColor: 'green',
+                  height: '100px',
+                  width: '20%',
                 }}
               >
                 3
               </Box>
               <Box
                 sx={{
-                  backgroundColor: "green",
-                  height: "100px",
-                  width: "20%",
+                  backgroundColor: 'green',
+                  height: '100px',
+                  width: '20%',
                 }}
               >
                 4
@@ -97,34 +95,29 @@ export default ({ sx }) => {
               <Box
                 sx={{
                   backgroundImage: `url('https://trvis.r10s.com/d/strg/ctrl/26/91d046af12015df16c75e62ab5eebe05154f4a0b.47.1.26.2.jpg')`,
-                  backgroundSize: "cover",
-                  backgroundPosition: "center",
-                  height: "100px",
-                  width: "20%",
-                  borderRadius: "0 0 1rem 0",
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                  height: '100px',
+                  width: '20%',
+                  borderRadius: '0 0 1rem 0',
                 }}
               >
                 <div
                   style={{
                     backgroundColor: color.black50,
-                    height: "100%",
-                    width: "100%",
+                    height: '100%',
+                    width: '100%',
 
-                    borderRadius: "0 0 1rem 0",
+                    borderRadius: '0 0 1rem 0',
 
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "center",
-                    alignItems: "center",
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    alignItems: 'center',
                   }}
                 >
-                  <InsertPhotoIcon
-                    sx={{ color: color.white, fontSize: "1rem" }}
-                  />
-                  <Typography
-                    variant="caption"
-                    sx={{ color: color.white, fontSize: "0.8rem" }}
-                  >
+                  <InsertPhotoIcon sx={{ color: color.white, fontSize: '1rem' }} />
+                  <Typography variant="caption" sx={{ color: color.white, fontSize: '0.8rem' }}>
                     瀏覽所有照片
                   </Typography>
                 </div>
@@ -135,18 +128,18 @@ export default ({ sx }) => {
         <Box
           sx={{
             border: `3px solid ${color.lightGrey}`,
-            borderRadius: "1rem",
-            padding: "1.1rem",
-            width: "45%",
+            borderRadius: '1rem',
+            padding: '1.1rem',
+            width: '45%',
           }}
         >
           <Stack direction="column">
             <div
               style={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-                gap: "1rem",
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                gap: '1rem',
               }}
             >
               <CircularProgressWithLabel
@@ -154,48 +147,43 @@ export default ({ sx }) => {
                 value={5}
                 thickness={5}
                 sx={{ color: color.accentGreen }}
-                text_sx={{ fontSize: "2rem" }}
+                text_sx={{ fontSize: '2rem' }}
               />
               <div>
                 <Typography variant="subtitle1" fontWeight="bold">
                   5 Exceptional
                 </Typography>
-                <Typography
-                  variant="subtitle2"
-                  sx={{ color: color.textDeepGrey }}
-                >
+                <Typography variant="subtitle2" sx={{ color: color.textDeepGrey }}>
                   來自認證住客的 24 則評價
                 </Typography>
               </div>
             </div>
-            <div style={{ display: "flex", flexDirection: "row" }}>
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
               <div style={{ flexGrow: 1 }}></div>
               <Button>查看更多詳細資訊</Button>
             </div>
           </Stack>
-          <Divider sx={{ marginTop: "1rem" }} />
-          <Stack direction="column" sx={{ marginTop: "1rem" }}>
+          <Divider sx={{ marginTop: '1rem' }} />
+          <Stack direction="column" sx={{ marginTop: '1rem' }}>
             <div>
               <Typography variant="body2" fontSize="0.8rem">
-                這棟別墅是別墅風格的夏威夷公寓出租。 所有 12
-                棟別墅均設有溫水游泳池，私密性強。
-                廚房、家具、家電一應俱全，可長期入住。
-                在您逗留期間，這將是一個只屬於您的私人空間，所以請隨意使用。
+                這棟別墅是別墅風格的夏威夷公寓出租。 所有 12 棟別墅均設有溫水游泳池，私密性強。
+                廚房、家具、家電一應俱全，可長期入住。 在您逗留期間，這將是一個只屬於您的私人空間，所以請隨意使用。
               </Typography>
             </div>
-            <div style={{ display: "flex", flexDirection: "row" }}>
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
               <div style={{ flexGrow: 1 }}></div>
               <Button>查看更多詳細資訊</Button>
             </div>
           </Stack>
-          <Divider sx={{ marginTop: "1rem" }} />
+          <Divider sx={{ marginTop: '1rem' }} />
           <Stack direction="column">
             <div
               style={{
-                display: "flex",
-                flexDirection: "column",
-                gap: "0.5rem",
-                marginTop: "1rem",
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '0.5rem',
+                marginTop: '1rem',
               }}
             >
               <div>
@@ -205,70 +193,64 @@ export default ({ sx }) => {
               </div>
               <div
                 style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  gap: "1rem",
+                  display: 'flex',
+                  flexDirection: 'row',
+                  gap: '1rem',
                 }}
               >
                 <div
                   style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    gap: "0.3rem",
-                    alignItems: "baseline",
+                    display: 'flex',
+                    flexDirection: 'row',
+                    gap: '0.3rem',
+                    alignItems: 'baseline',
                   }}
                 >
-                  <DoneIcon style={{ fontSize: "1rem" }} />
+                  <DoneIcon style={{ fontSize: '1rem' }} />
                   <Typography variant="body1">度假村酒店</Typography>
                 </div>
                 <div
                   style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    gap: "0.3rem",
-                    alignItems: "baseline",
+                    display: 'flex',
+                    flexDirection: 'row',
+                    gap: '0.3rem',
+                    alignItems: 'baseline',
                   }}
                 >
-                  <DoneIcon style={{ fontSize: "1rem" }} />
+                  <DoneIcon style={{ fontSize: '1rem' }} />
                   <Typography variant="body1">停車場</Typography>
                 </div>
               </div>
             </div>
-            <div style={{ display: "flex", flexDirection: "row" }}>
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
               <div style={{ flexGrow: 1 }}></div>
               <Button>查看更多詳細資訊</Button>
             </div>
           </Stack>
 
-          <Divider sx={{ marginTop: "1rem" }} />
+          <Divider sx={{ marginTop: '1rem' }} />
 
-          <Stack direction="row" spacing={3} sx={{ marginTop: "1rem" }}>
+          <Stack direction="row" spacing={3} sx={{ marginTop: '1rem' }}>
             <div
               style={{
                 backgroundImage: `url('https://media.wired.com/photos/59269cd37034dc5f91bec0f1/master/pass/GoogleMapTA.jpg')`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
 
-                width: "50%",
-                minWidth: "150px",
-                height: "200px",
-                borderRadius: "1rem",
+                width: '50%',
+                minWidth: '150px',
+                height: '200px',
+                borderRadius: '1rem',
               }}
             ></div>
 
-            <Box sx={{ padding: "1rem" }}>
-              <Typography
-                variant="subtitle1"
-                fontWeight="bold"
-                sx={{ color: color.textDeepGreen }}
-              >
+            <Box sx={{ padding: '1rem' }}>
+              <Typography variant="subtitle1" fontWeight="bold" sx={{ color: color.textDeepGreen }}>
                 日本, 906-0203, 935-8 Ueno Miyaguni, Miyakojima-shi, Okinawa
               </Typography>
               <ul>
                 <li>
-                  <Typography variant="body2">
-                    由宮古機場駕車約 15 分鐘可達
-                  </Typography>
+                  <Typography variant="body2">由宮古機場駕車約 15 分鐘可達</Typography>
                 </li>
               </ul>
             </Box>
