978c4a9c41e4d1522cf22e9314cae5caa88d77e1
louiscklaw <louiscklaw@users.noreply.github.com>
2023-01-15 20:12:17 +0800

    fix unused package,

--- rakuten-travel-tryout/backend/prisma/migrations/20230114085637_/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "HotelInfo" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "email" TEXT NOT NULL,
-    "name" TEXT
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "HotelInfo_email_key" ON "HotelInfo"("email");
