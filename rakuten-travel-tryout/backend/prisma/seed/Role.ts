32477f4b8165f25230309406c29d5f0db37789d0
louiscklaw <louiscklaw@users.noreply.github.com>
2023-01-16 22:18:42 +0800

    update,

--- rakuten-travel-tryout/backend/prisma/seed/Role.ts
+++ rakuten-travel-tryout/backend/prisma/seed/Role.ts
@@ -4,16 +4,28 @@ const prisma = new PrismaClient();
 async function Role() {
   process.stdout.write('seeding Role ...');
 
-  for (var i = 0; i < 19; i++) {
+  let role_list = [
+    //
+    'admin',
+    'super user',
+    'hotel manager',
+    'hotel operator',
+    'client',
+  ];
+
+  for (var i = 0; i < role_list.length; i++) {
     const alice = await prisma.role.upsert({
-      where: { id: i },
+      where: { id: i + 1 },
       update: {},
       create: {
-        name: `alice${i}@Role.io`,
-        description: 'Alice',
+        name: role_list[i],
+        description: `${role_list[i]} description`,
       },
     });
   }
+
+  let result = await prisma.role.findMany({});
+  console.log(JSON.stringify(result, null, 2));
 
   await prisma.$disconnect();
   console.log('seed Role done');
