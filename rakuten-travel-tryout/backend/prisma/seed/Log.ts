8db77c639d6cb0d3bfe35f0a4e51c41659d83630
louiscklaw <louiscklaw@users.noreply.github.com>
2023-01-16 22:07:00 +0800

    update,

--- rakuten-travel-tryout/backend/prisma/seed/Log.ts
+++ rakuten-travel-tryout/backend/prisma/seed/Log.ts
@@ -4,17 +4,6 @@ const prisma = new PrismaClient();
 async function Log() {
   process.stdout.write('seeding log ...');
   let list = ['log1', 'log2', 'log3'];
-  let list_serveties = ['LOW', 'MID', 'HIGH'];
-
-  for (var i = 0; i < list_serveties.length; i++) {
-    await prisma.serverity.upsert({
-      where: { id: i + 1 },
-      update: {},
-      create: {
-        category: list_serveties[i],
-      },
-    });
-  }
 
   for (var i = 0; i < list.length; i++) {
     const alice = await prisma.log.upsert({
@@ -23,20 +12,12 @@ async function Log() {
       create: {
         name: `alice${i}@log.io`,
         description: `${list[i]}`,
-        serverity: {
-          create: [{ servity: { connect: { id: 1 } } }],
-        },
+        serverity: 'LOW',
       },
     });
   }
 
-  let result_log = await prisma.log.findMany({
-    include: {
-      serverity: {
-        include: { servity: true },
-      },
-    },
-  });
+  let result_log = await prisma.log.findMany({});
   // console.log(JSON.stringify(result_log, null, 2));
 
   await prisma.$disconnect();
