a1fcab7ba61d8a6c46f5018bec2fbb6c7d64a055
louiscklaw <louiscklaw@users.noreply.github.com>
2023-01-16 12:20:31 +0800

    update,

--- rakuten-travel-tryout/backend/prisma/seed/HotelInfo.ts
+++ rakuten-travel-tryout/backend/prisma/seed/HotelInfo.ts
@@ -25,6 +25,20 @@ async function HotelInfo() {
         fax: '+81 980 74 7501',
         accommodation_notice: '如果入住時間超過預定時間，請務必與我們聯繫。',
         enabled: true,
+        awards: {
+          create: [{ name: 'Rakuten Travel Gold Award 2021' }, { name: 'Rakuten Travel Gold Award 2020' }],
+        },
+        covin_practice: {
+          create: [
+            { name: '員工', description: '服務客人時保持適當距離 員工洗手、漱口、戴口罩 定期實施體溫檢測' },
+            { name: '消毒', description: '公共區域定期消毒 消毒客房 即棄式拖鞋' },
+            { name: '通風', description: '公共區域定期換氣 客房在使用前後換氣' },
+            { name: '登記入住／退房', description: '房間內辦理入住 房間內辦理退房' },
+            { name: '客房', description: '提供消毒液 提供口罩 定期消毒家具' },
+            { name: '緊急應對的準備', description: '已備妥新冠肺炎緊急對應手冊 員工已接受新冠肺炎緊急對應訓練' },
+          ],
+        },
+        facilities: { create: [{ name: '停車場' }] },
       },
     });
   }
