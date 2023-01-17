a9bb0cab54aa5b6830c28200631400165d0b8fba
louiscklaw <louiscklaw@users.noreply.github.com>
2023-01-17 22:53:21 +0800

    update,

--- rakuten-travel-tryout/backend/routes/index.ts
+++ rakuten-travel-tryout/backend/routes/index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 
-const helloworld = (app: Express, prisma: PrismaClient) => {
+const helloworld = (app: Express) => {
   app.get('/helloworld', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server helloworld');
   });
@@ -9,50 +9,50 @@ const helloworld = (app: Express, prisma: PrismaClient) => {
   console.log('root route initialized');
 
   app.get('/feed', async (req, res) => {
+    const prisma = new PrismaClient();
     const posts = await prisma.post.findMany({
       where: { published: true },
       include: { author: true },
     });
 
+    prisma.$disconnect();
     res.json(posts);
   });
 
   app.post('/post', async (req, res) => {
     const { title, content, authorEmail } = req.body;
-
+    const prisma = new PrismaClient();
     const post = await prisma.post.create({
       data: {
         title,
-
         content,
-
         published: false,
-
         author: { connect: { email: authorEmail } },
       },
     });
-
+    prisma.$disconnect();
     res.json(post);
   });
 
   app.put('/publish/:id', async (req, res) => {
     const { id } = req.params;
-
+    const prisma = new PrismaClient();
     const post = await prisma.post.update({
       where: { id: +id },
       data: { published: true },
     });
-
+    prisma.$disconnect();
     res.json(post);
   });
 
   app.delete('/user/:id', async (req, res) => {
     const { id } = req.params;
-
+    const prisma = new PrismaClient();
     const user = await prisma.user.delete({
       where: { id: +id },
     });
 
+    prisma.$disconnect();
     res.json(user);
   });
 };
