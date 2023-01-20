84e5b5f1094aff4ddefea8c1bc689ec4c0c1cf1b
louiscklaw <louiscklaw@users.noreply.github.com>
2023-01-21 03:32:16 +0800

    update,

---
+++ rakuten-travel-tryout/nextjs-helloworld-tryout/components/pages/BrokenLink/index.jsx
@@ -0,0 +1,76 @@
+import { Redirect, Route } from 'react-router-dom';
+import { IonRouterOutlet, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { cog, flash, list } from 'ionicons/icons';
+
+import Home from '../Feed';
+import Lists from '../Lists';
+import ListDetail from '../ListDetail';
+import Settings from '../Settings';
+import { useTranslation } from 'react-i18next';
+import { IonText } from '@ionic/react';
+import { warning } from 'ionicons/icons';
+
+import ErrorSvg from './error.svg';
+
+const BrokenLink = () => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <div
+        style={{
+          height: '100vh',
+          // backgroundImage: `linear-gradient(0deg,  #3f87a6, #ebf8e1,url("https://source.unsplash.com/random/900x700/?lobby")`,
+          // backgroundImage:
+          // 'linear-gradient(0.25turn, #3f87a6, #ebf8e1, url("https://images.unsplash.com/photo-1599392687131-d82ad47a871f"))',
+          backgroundImage: 'url("https://images.unsplash.com/photo-1599392687131-d82ad47a871f")',
+
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: 'rgba(128,0,0,0.5)',
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          {/* <ErrorSvg width="25vw" height="25vw" fill={'#fff'} fillOpacity={0.8} /> */}
+          {/* <img
+            src={ErrorSvg.src}
+            width="50%"
+            style={{
+              fill: '#ff0000',
+            }}
+          /> */}
+
+          <div
+            style={{
+              backgroundImage: 'url(' + ErrorSvg.src + ')',
+              height: '50vw',
+              width: '50vw',
+              backgroundSize: 'contain',
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'center',
+            }}
+          ></div>
+
+          <IonText style={{ color: '#ffffff' }}>
+            <h1>{t('Sorry')}</h1>
+          </IonText>
+          <IonText style={{ color: '#ffffff' }}>
+            <p>{t('This restaurant is no longer subscribed to our service.')}</p>
+          </IonText>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BrokenLink;
